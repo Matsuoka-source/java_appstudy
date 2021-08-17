11671742ecf54e21406dfe4c57c1ae2b2b03193d
Matsuoka-source <tm19930425@gmail.com>
2021-08-17 16:07:54 +0900

    attach

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -3,20 +3,31 @@ package p1;
 public class Class1 {
 
 	public static void main(String[] args) {
+		
+		System.out.println("モンスターが現れた。");
+		System.out.println("--------------------");
+		System.out.println("おばけ       HP: 60 ");
+		System.out.println("スライム     HP: 120");
+		System.out.println("--------------------");
+		System.out.println("勇者の攻撃");
+		System.out.println("--------------------");
+		System.out.println("おばけ       HP: 40 ");
+		System.out.println("スライム     HP: 120");
+		System.out.println("--------------------");
 
-		String m = "スライム";
-		String p = "勇者";
+		// String m = "スライム";
+		// String p = "勇者";
 
-		System.out.println(m + "が現れた");
-		System.out.println(p + "は" + m + "に攻撃");
-		System.out.println(m + "は250のダメージ");
-		System.out.println(m + "を倒した");
+		// System.out.println(m + "が現れた");
+		// System.out.println(p + "は" + m + "に攻撃");
+		// System.out.println(m + "は250のダメージ");
+		// System.out.println(m + "を倒した");
 		// パラメーターの表示
-		System.out.println("----------------------------");
-		System.out.println("なまえ		HP	MP");
-		System.out.println("ゆうと		350	30");
-		System.out.println("かぐら		200	140");
-		System.out.println("----------------------------");
+		// System.out.println("----------------------------");
+		// System.out.println("なまえ		HP	MP");
+		// System.out.println("ゆうと		350	30");
+		// System.out.println("かぐら		200	140");
+		// System.out.println("----------------------------");
 
 	}
 
