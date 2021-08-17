d564eeeb7a028de5100e32e4008887824126b364
Matsuoka-source <tm19930425@gmail.com>
2021-08-17 15:42:50 +0900

    app study

---
+++ src/p1/Class1.java
@@ -0,0 +1,23 @@
+package p1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+
+		String m = "スライム";
+		String p = "勇者";
+
+		System.out.println(m + "が現れた");
+		System.out.println(p + "は" + m + "に攻撃");
+		System.out.println(m + "は250のダメージ");
+		System.out.println(m + "を倒した");
+		// パラメーターの表示
+		System.out.println("----------------------------");
+		System.out.println("なまえ		HP	MP");
+		System.out.println("ゆうと		350	30");
+		System.out.println("かぐら		200	140");
+		System.out.println("----------------------------");
+
+	}
+
+}
