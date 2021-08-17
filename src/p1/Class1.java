85a55c4405987a21bf103e6d854c64a22df99a09
Matsuoka-source <tm19930425@gmail.com>
2021-08-17 20:24:50 +0900

    add

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -3,16 +3,30 @@ package p1;
 public class Class1 {
 
 	public static void main(String[] args) {
-		
+
+		String m1 = "おばけ";
+		String m2 = "スライム";
+		String m3 = "箱太郎";
+
+		int hp1 = 60;
+		int hp2 = 120;
+		int hp3 = 280;
+
 		System.out.println("モンスターが現れた。");
 		System.out.println("--------------------");
-		System.out.println("おばけ       HP: 60 ");
-		System.out.println("スライム     HP: 120");
+		System.out.println(m1 + "       HP: " + hp1);
+		System.out.println(m2 + "     HP: " + hp2);
+		System.out.println(m3 + "       HP: " + hp3);
 		System.out.println("--------------------");
 		System.out.println("勇者の攻撃");
+		System.out.println("おばけに20のダメージ");
+		
+		hp1 = hp1-20;
+		
 		System.out.println("--------------------");
-		System.out.println("おばけ       HP: 40 ");
-		System.out.println("スライム     HP: 120");
+		System.out.println(m1 + "       HP: " + hp1);
+		System.out.println(m2 + "     HP: " + hp2);
+		System.out.println(m3 + "       HP: " + hp3);
 		System.out.println("--------------------");
 
 		// String m = "スライム";
@@ -24,9 +38,9 @@ public class Class1 {
 		// System.out.println(m + "を倒した");
 		// パラメーターの表示
 		// System.out.println("----------------------------");
-		// System.out.println("なまえ		HP	MP");
-		// System.out.println("ゆうと		350	30");
-		// System.out.println("かぐら		200	140");
+		// System.out.println("なまえ HP MP");
+		// System.out.println("ゆうと 350 30");
+		// System.out.println("かぐら 200 140");
 		// System.out.println("----------------------------");
 
 	}
