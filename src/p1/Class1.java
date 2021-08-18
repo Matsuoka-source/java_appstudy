680a1618b8b5608a0c06507ac592fe5784aa62fc
Matsuoka-source <tm19930425@gmail.com>
2021-08-18 09:54:24 +0900

    add

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -6,7 +6,11 @@ public class Class1 {
 
 	public static void main(String[] args) {
 
-		// キャラクター名
+		// 主人公キャラ
+		String p1 = "勇者";
+		String p2 = "魔法使い";
+
+		// 敵キャラクター名
 		String m1 = "おばけ";
 		String m2 = "スライム";
 		String m3 = "箱太郎";
@@ -20,6 +24,8 @@ public class Class1 {
 		int da1 = new Random().nextInt(40);
 		int da2 = new Random().nextInt(30);
 
+		// バトルの開始
+		// パラメーターの表示
 		System.out.println("モンスターが現れた。");
 		System.out.println("--------------------");
 		System.out.println(m1 + "       HP: " + hp1);
@@ -28,41 +34,44 @@ public class Class1 {
 		System.out.println("--------------------");
 
 		// 勇者の攻撃は単体
-		System.out.println("勇者の攻撃!");
+		System.out.println(p1 + "の攻撃!");
 		System.out.println("おばけに" + da1 + "のダメージ");
 		hp1 = hp1 - da1;
 
-		System.out.println("魔法使いの攻撃!");
-		// 魔法使いの攻撃は全体
+		// 魔法使いの攻撃は全体(ダメージ量一律)
+		System.out.println(p2 + "の攻撃!");
 		System.out.println(m1 + "に" + da2 + "のダメージ");
-		hp1 = hp1 - da2;
-
 		System.out.println(m2 + "に" + da2 + "のダメージ");
-		hp2 = hp2 - da2;
-
 		System.out.println(m3 + "に" + da2 + "のダメージ");
+		hp1 = hp1 - da2;
+		hp2 = hp2 - da2;
 		hp3 = hp3 - da2;
 
+		// パラメーターの表示
 		System.out.println("--------------------");
 		System.out.println(m1 + "       HP: " + hp1);
 		System.out.println(m2 + "     HP: " + hp2);
 		System.out.println(m3 + "       HP: " + hp3);
 		System.out.println("--------------------");
+
+		if (hp1 <= 0) {
+			System.out.println(m1 + "は倒れた!!");
+		}
 
 		// 回復の乱数
-		int heal = new Random().nextInt(30);
-
-		System.out.println("おばけの回復魔法！");
-		System.out.println("おばけはHPを" + heal + "回復した。");
-		hp1 = hp1 + heal;
-
-		System.out.println("--------------------");
-		System.out.println(m1 + "       HP: " + hp1);
+		// int heal = new Random().nextInt(30);
+		//
+		// System.out.println("おばけの回復魔法！");
+		// System.out.println("おばけはHPを" + heal + "回復した。");
+		// hp1 = hp1 + heal;
+		//
+		// System.out.println("--------------------");
+		// System.out.println(m1 + " HP: " + hp1);
 		// (hp1 + heal)でも可能 ただし順序に違和感を感じるので別で計算式を記述
-		System.out.println(m2 + "     HP: " + hp2);
-		System.out.println(m3 + "       HP: " + hp3);
-		System.out.println("--------------------");
-
+		// System.out.println(m2 + " HP: " + hp2);
+		// System.out.println(m3 + " HP: " + hp3);
+		// System.out.println("--------------------");
+		//
 		// String m = "スライム";
 		// String p = "勇者";
 
